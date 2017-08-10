00c478f6874ee83bfa2434ca250f38d11b01616a
lxg <18675754250@sina.cn>
2017-08-10 22:45:52 +0800

    修复验证拦截失败

--- Guardian.js
+++ Guardian.js
@@ -1,83 +1,84 @@
 var Guardian = {
 
-	init:function (obj,btn){
-		var InputData = [];
-		obj.each(function(){
-			InputData = $(this).find('input');
-		})
-		for(var i = 0; i < InputData.length; i++){
-			var inputType = InputData[i].getAttribute('inputType');
-			if(inputType == 'on'){
-				var InputName = InputData[i].getAttribute('name');
-				this.Ready(InputName);
-			}
-		}
-		this.sub(obj,btn);
-	},
+    init:function (obj,btn){
+        var InputData = [];
+        obj.each(function(){
+            InputData = $(this).find('input');
+        })
+        for(var i = 0; i < InputData.length; i++){
+            var inputType = InputData[i].getAttribute('inputType');
+            if(inputType == 'on'){
+                var InputName = InputData[i].getAttribute('name');
+                this.Ready(InputName);
+            }
+        }
+        this.sub(obj,btn);
+    },
 
-	Ready:function (InputName,code_info = 'true'){
-		Name = $('input[name="'+InputName+'"]');
+    Ready:function (InputName,code_info = 'true'){
+        Name = $('input[name="'+InputName+'"]');
 
-		if(Name.attr('validate') == 'on'){
-			Guardian.InputBlur(Name,code_info);
-		}
-	},
+        if(Name.attr('validate') == 'on'){
+            Guardian.InputBlur(Name,code_info);
+        }
+    },
 
-	InputBlur:function (obj,code_info='true'){
-		if(code_info){
-			obj.blur(function(){
-				var InputValidate = obj.attr('validate-method');
-				var InputEmpty = obj.attr('empty-data');
-				var InputError = obj.attr('error-data');
-				if(obj.val() == ''){
-					Guardian.sub_error(obj,InputEmpty)
-				}else{
-					var CodeInfo = Guardian.FormatCode(obj.val(),InputValidate);
-					if(CodeInfo){
-						Guardian.sub_info(obj,'')	
-					}else{
-						Guardian.sub_error(obj,InputError)
-					}
-				}
-			})
-		}else{
-			var InputValidate = obj.attr('validate-method');
-			var InputEmpty = obj.attr('empty-data');
-			var InputError = obj.attr('error-data');
-			if(obj.val() == ''){
-				Guardian.sub_error(obj,InputEmpty)
-			}else{
-				var CodeInfo = Guardian.FormatCode(obj.val(),InputValidate);
-				if(CodeInfo){
-					Guardian.sub_info(obj,'')	
-				}else{
-					Guardian.sub_error(obj,InputError)
-				}
-			}	
-		}
-	},
+    InputBlur:function (obj,code_info='true'){
+        if(code_info){
+            obj.blur(function(){
+                var InputValidate = obj.attr('validate-method');
+                var InputEmpty = obj.attr('empty-data');
+                var InputError = obj.attr('error-data');
+                if(obj.val() == ''){
+                    Guardian.sub_error(obj,InputEmpty)
+                }else{
+                    var CodeInfo = Guardian.FormatCode(obj.val(),InputValidate);
+                    if(CodeInfo){
+                        Guardian.sub_info(obj,'')
+                    }else{
+                        Guardian.sub_error(obj,InputError)
+                    }
+                }
+            })
+        }else{
+            var InputValidate = obj.attr('validate-method');
+            var InputEmpty = obj.attr('empty-data');
+            var InputError = obj.attr('error-data');
+            if(obj.val() == ''){
+                Guardian.sub_error(obj,InputEmpty)
+            }else{
+                var CodeInfo = Guardian.FormatCode(obj.val(),InputValidate);
+                if(CodeInfo){
+                    Guardian.sub_info(obj,'')
+                }else{
+                    Guardian.sub_error(obj,InputError)
+                }
+            }
+        }
+    },
 
-	sub_error:function (obj,info){
-		obj.attr('return','0');
-	    obj.css({'border':'1px solid #a94442'});
-        var span_ = $("<span></span>").html(info);
+    sub_error:function (obj,info){
+        obj.attr('return','0');
+        obj.css({'border':'1px solid #e73d4a'});
+        var span_ = $("<span style='margin-top: 5px; margin-bottom:" +
+            " 5px;color:red;display:block;font-size:13px;'></span>").html(info);
         obj.parent('div').children('span').remove();
         if(obj.next('span').size() < 1){
             obj.parent('div').append(span_);
         }
-	    obj.parent('div').addClass('guardian');
+        obj.parent('div').addClass('guardian');
 
-	},
+    },
 
-	sub_info:function (obj,info){
-		obj.attr('return','1');
-	    obj.css({'border':'1px solid #27d'});
+    sub_info:function (obj,info){
+        obj.attr('return','1');
+        obj.css({'border':'1px solid #27d'});
         obj.parent('div').children('span').remove();
-	    obj.parent('div').removeClass('guardian');
-	},
+        obj.parent('div').removeClass('guardian');
+    },
 
-	FormatCode:function (con, arr_of){
-		var arr = new Array();
+    FormatCode:function (con, arr_of){
+        var arr = new Array();
         arr['0'] = /^[\u4E00-\u9FA5·0-9A-z]{2,}$/;
         arr['1'] = /^[a-z0-9_-]{3,16}$/;
         arr['2'] = /^[a-zA-Z-]{3,16}$/;
@@ -90,7 +91,7 @@ var Guardian = {
         arr['9'] = /^(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)$/;
         arr['10'] = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
         arr['11'] = /^[1-9]\d*$/;
-        arr['12'] = /^-[1-9]\d*$/;
+        arr['12'] = /^-[1-9]\d*$ /;
 
         if(typeof arr_of == 'object'){
             var str = new Array();
@@ -113,152 +114,147 @@ var Guardian = {
             }else{
                 return true;
             }
-        };	
-	},
+        };
+    },
 
-	Ckeck:function(InputName){
-		var Name = $('input[name="'+InputName+'"]');
+    Ckeck:function(InputName){
+        var Name = $('input[name="'+InputName+'"]');
 
-		if(Name.attr('validate') == 'on'){
-			var inputType = Name.attr('type');
-			if(inputType == 'checkbox'){
-				var Name = $('input[name="'+InputName+'"]');
-				this.ManyCkeck(Name);
-			}else if(inputType == 'radio'){
-				var Name = $('input[name="'+InputName+'"]');
-				this.RadioCkeck(Name);
-			}
-		}
-		
-	},
+        if(Name.attr('validate') == 'on'){
+            var inputType = Name.attr('type');
+            if(inputType == 'checkbox'){
+                var Name = $('input[name="'+InputName+'"]');
+                this.ManyCkeck(Name);
+            }else if(inputType == 'radio'){
+                var Name = $('input[name="'+InputName+'"]');
+                this.RadioCkeck(Name);
+            }
+        }
 
-
-	ManyCkeck:function(InputName){
-		var min = InputName.attr('Checkmin');
-		var max = InputName.attr('Checkmax');
-		var Name = InputName.attr('name');
-		var InputEmpty = InputName.attr('empty-data');
-		var InputError = InputName.attr('error-data');
-		var len = $('input[name="'+Name+'"]:checked').length;
-		if(len < 1){
-			this.sub_error(InputName,InputEmpty);
-		}else{
-			if(len < min || len > max){
-				if(InputError){
-					this.sub_error(InputName,InputError);	
-				}else{
-					this.sub_error(InputName,InputEmpty);
-				}
-			}else{
-				this.sub_info(InputName,'')
-			}
-		}
-	},
-
-	RadioCkeck:function(InputName){
-		var Name = InputName.attr('name');
-		var InputEmpty = InputName.attr('empty-data');
-		var InputError = InputName.attr('error-data');
-		var len = $('input[name="'+Name+'"]:checked').length;
-		if(len == 0){
-			if(InputError){
-				this.sub_error(InputName,InputError);
-			}else{
-				this.sub_error(InputName,InputEmpty);
-			}
-		}else{
-			this.sub_info(InputName,'');
-		}
-	},
-
-	Select:function(obj){
-		obj.each(function(){
-			InputSelect = $(this).find('select');
-		})
-		for(var i = 0; i < InputSelect.length; i++){
-			var validate = InputSelect[i].getAttribute('validate');
-			if(validate == 'on'){
-				var InputName = InputSelect[i].getAttribute('name');
-				this.SelectCode(InputName);
-			}
-		}
-		
-	},
-
-	SelectCode:function(InputName){
-		var InputName = $('select[name="'+InputName+'"]');
-		var len = InputName.val();
-		var InputEmpty = InputName.attr('empty-data');
-		var InputError = InputName.attr('error-data');
-		if(!len){
-			if(InputError){
-				this.sub_error(InputName,InputError);
-			}else{
-				this.sub_error(InputName,InputEmpty);
-			}
-		}else{
-			this.sub_info(InputName,'');
-		}
-	},
+    },
 
 
-	Textarea:function(obj){
-		obj.each(function(){
-			Inputtextarea = $(this).find('textarea');
-		})
-		for(var i = 0; i < Inputtextarea.length; i++){
-			var validate = Inputtextarea[i].getAttribute('validate');
-			if(validate == 'on'){
-				var InputName = Inputtextarea[i].getAttribute('name');
-				this.TextareaCode(InputName);
-			}
-		}
-	},
+    ManyCkeck:function(InputName){
+        var min = InputName.attr('Checkmin');
+        var max = InputName.attr('Checkmax');
+        var Name = InputName.attr('name');
+        var InputEmpty = InputName.attr('empty-data');
+        var InputError = InputName.attr('error-data');
+        var len = $('input[name="'+Name+'"]:checked').length;
+        if(len < 1){
+            this.sub_error(InputName,InputEmpty);
+        }else{
+            if(len < min || len > max){
+                this.sub_error(InputName,InputEmpty);
+            }else{
+                this.sub_info(InputName,'')
+            }
+        }
+    },
 
-	TextareaCode:function(InputName){
-		var InputName= $('textarea[name="'+InputName+'"]');
-		var InputValidate = InputName.attr('validate-method');
-		var InputEmpty = InputName.attr('empty-data');
-		var InputError = InputName.attr('error-data');
-		if(InputName.val() == ''){
-			Guardian.sub_error(InputName,InputEmpty)	
-		}else{
-			var CodeInfo = Guardian.FormatCode(InputName.val(),InputValidate);
-			if(CodeInfo){
-				Guardian.sub_info(InputName,'')	
-			}else{
-				Guardian.sub_error(InputName,InputError)
-			}
-		}
-		
-	},
+    RadioCkeck:function(InputName){
+        var Name = InputName.attr('name');
+        var InputEmpty = InputName.attr('empty-data');
+        var InputError = InputName.attr('error-data');
+        var len = $('input[name="'+Name+'"]:checked').length;
+        if(len == 0){
+            if(InputError){
+                this.sub_error(InputName,InputError);
+            }else{
+                this.sub_error(InputName,InputEmpty);
+            }
+        }else{
+            this.sub_info(InputName,'');
+        }
+    },
 
-	sub:function(obj,btn){
+    Select:function(obj){
+        obj.each(function(){
+            InputSelect = $(this).find('select');
+        })
+        for(var i = 0; i < InputSelect.length; i++){
+            var validate = InputSelect[i].getAttribute('validate');
+            if(validate == 'on'){
+                var InputName = InputSelect[i].getAttribute('name');
+                this.SelectCode(InputName);
+            }
+        }
+
+    },
+
+    SelectCode:function(InputName){
+        var InputName = $('select[name="'+InputName+'"]');
+        var len = InputName.val();
+        var InputEmpty = InputName.attr('empty-data');
+        var InputError = InputName.attr('error-data');
+        if(!len){
+            if(InputError){
+                this.sub_error(InputName,InputError);
+            }else{
+                this.sub_error(InputName,InputEmpty);
+            }
+        }else{
+            this.sub_info(InputName,'');
+        }
+    },
+
+
+    Textarea:function(obj){
+        obj.each(function(){
+            Inputtextarea = $(this).find('textarea');
+        })
+        for(var i = 0; i < Inputtextarea.length; i++){
+            var validate = Inputtextarea[i].getAttribute('validate');
+            if(validate == 'on'){
+                var InputName = Inputtextarea[i].getAttribute('name');
+                this.TextareaCode(InputName);
+            }
+        }
+    },
+
+    TextareaCode:function(InputName){
+        var InputName= $('textarea[name="'+InputName+'"]');
+        var InputValidate = InputName.attr('validate-method');
+        var InputEmpty = InputName.attr('empty-data');
+        var InputError = InputName.attr('error-data');
+        if(InputName.val() == ''){
+            Guardian.sub_error(InputName,InputEmpty)
+        }else{
+            var CodeInfo = Guardian.FormatCode(InputName.val(),InputValidate);
+            if(CodeInfo){
+                Guardian.sub_info(InputName,'')
+            }else{
+                Guardian.sub_error(InputName,InputError)
+            }
+        }
+
+    },
+
+    sub:function(obj,btn){
         btn.click(function(){
-			obj.each(function(){
-				InputText = $(this).find('input');
-			})
-			for(var i = 0; i < InputText.length; i++){
-				var inputType = InputText[i].getAttribute('inputType');
-				if(inputType == 'on'){
-					var InputName = InputText[i].getAttribute('name');
-					Guardian.Ready(InputName,false);
-				}else{
-					var InputName = InputText[i].getAttribute('name');
-					Guardian.Ckeck(InputName);
-				}
-			}
-			Guardian.Select(obj);
-			Guardian.Textarea(obj);
-
-			if($('.guardian').size()){
+            obj.each(function(){
+                InputText = $(this).find('input');
+            })
+            for(var i = 0; i < InputText.length; i++){
+                var inputType = InputText[i].getAttribute('inputType');
+                if(inputType == 'on'){
+                    var InputName = InputText[i].getAttribute('name');
+                    Guardian.Ready(InputName,false);
+                }else{
+                    var InputName = InputText[i].getAttribute('name');
+                    Guardian.Ckeck(InputName);
+                }
+            }
+            Guardian.Select(obj);
+            Guardian.Textarea(obj);
+            if($('.guardian').size()){
                 $(this).prev('input[name="code_info"]').attr('code_info',0);
-				return false;
-			}else{
-				$(this).prev('input[name="code_info"]').attr('code_info',1);
-			}
-			
-		})
+                return false;
+            }else{
+                $(this).prev('input[name="code_info"]').attr('code_info',1);
+            }
 
-	}
+        })
+
+    }
 }
